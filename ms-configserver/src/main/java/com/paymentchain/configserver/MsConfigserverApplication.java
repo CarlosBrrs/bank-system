59779ecf5e3fc2023b5489325f7889c2edab3102
Carlos Barrios <carlosbarrios148@gmail.com>
2023-06-23 23:12:01 -0500

    Create configserver ms

---
+++ ms-configserver/src/main/java/com/paymentchain/configserver/MsConfigserverApplication.java
@@ -0,0 +1,18 @@
+package com.paymentchain.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class MsConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigserverApplication.class, args);
+	}
+
+
+}
